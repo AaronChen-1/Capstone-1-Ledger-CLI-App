c3945d0a1634d42e63dbc37db34ecda56a7cc207
arone415 <105476658+arone415@users.noreply.github.com>
2025-10-15 10:33:44 -0700

    Added all fields (data that defines a transaction)

--- src/main/java/com/financetracker/Transaction.java
+++ src/main/java/com/financetracker/Transaction.java
@@ -4,4 +4,9 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class Transaction {
+    private LocalDate date;
+    private LocalTime time;
+    private String description;
+    private String vendor;
+    private double amount;
 }
