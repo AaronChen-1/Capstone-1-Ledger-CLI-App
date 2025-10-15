8716cd920663146bcae840dcf52790896090b97b
arone415 <105476658+arone415@users.noreply.github.com>
2025-10-15 10:50:06 -0700

    Test code for Main.java(temporary)

--- src/main/java/com/financetracker/Main.java
+++ src/main/java/com/financetracker/Main.java
@@ -1,4 +1,15 @@
 package com.financetracker;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Main {
+    public static void main(String[] args) {
+        Transaction test = new Transaction(
+                LocalDate.now(), LocalTime.now(), "Test", "Test", 100.00
+        );
+
+        System.out.println(test);
+        System.out.println(test.toCSV());
+    }
 }
